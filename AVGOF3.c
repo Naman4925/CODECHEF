42e139cfaef1b07cc8ca12fb017a5a93d2ba2430
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-01-26 21:26:22 +0530

    Create AVGOF3.c

---
+++ AVGOF3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    int s;
+    scanf("%d",&t);
+    while(t!=0){
+        scanf("%d",&s);
+        printf("%d %d %d\n",s-1,s,s+1);
+        t--;
+    }
+    
+	// your code goes here
+	
+}
