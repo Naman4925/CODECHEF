5f9dd1ba47eabf7b017b9e5ab55c3754c35f13d8
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-01-27 10:33:05 +0530

    Create Validtriangles.cpp

---
+++ Validtriangles.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b,c;
+	cin>>t;
+	while(t--){
+	    cin>>a>>b>>c;
+	    if(a+b+c==180){
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
