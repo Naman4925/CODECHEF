72910cef52f870fcf7fc2f462fd43e4b4dd00d87
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-01-27 10:53:07 +0530

    Create Pakaging.cpp

---
+++ Pakaging.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    long long int N;
+    while(T--){
+        cin>>N;
+        cout<<N/2 +1<<endl;
+    }
+	// your code goes here
+	return 0;
+}
