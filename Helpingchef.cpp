6f8be7172056860268a2d9ed8fd37aa90a6a462e
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-01-27 10:31:22 +0530

    Create Helpingchef.cpp

---
+++ Helpingchef.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin>>t ;
+	while(t--){
+	    int a ;
+	    cin>>a ;
+	    if (a <10){
+	        cout<<"Thanks for helping Chef!"<<endl;
+	    }
+	    else{
+	        cout<<"-1"<<endl;
+	    }
+	}
+	return 0;
+}
