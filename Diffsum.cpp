27c3b00da472827f377979ebe449a2a63d7acc78
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-01-27 11:03:29 +0530

    Create Diffsum.cpp

---
+++ Diffsum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N1,N2;
+    cin>>N1>>N2;
+    if(N1>N2){
+        cout<<N1-N2<<endl;
+    }
+    else{
+        cout<<N1+N2<<endl;
+    }
+	// your code goes here
+	return 0;
+}
