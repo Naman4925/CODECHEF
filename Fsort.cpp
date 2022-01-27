6160ea0bf5431fc9bd4511c698d1ccc7be0f9941
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-01-27 10:23:57 +0530

    Create Fsort.cpp

---
+++ Fsort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+   int t;
+   cin >> t;
+//   if (t >= 1 && t <= 20)
+//   {
+      while (t--)
+      {
+         int n;
+         cin >> n;
+        //  if(n>=1 && n<=10000)
+        //  {
+            cout << int(sqrt(n)) << endl;
+        //  }
+         
+      }
+//   }
+
+   return 0;
+}
