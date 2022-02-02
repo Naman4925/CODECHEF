a07aac5fb30c4f8067d6d867894d48c0c27db511
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-02-02 20:33:50 +0530

    Create Avoid_Contact.c

---
+++ Avoid_Contact.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x,y;
+	    scanf("%d %d",&x,&y);
+	    if(x==y)
+	        printf("%d\n",2*x-1);
+	    else{
+	        printf("%d\n",x+y);
+	    }
+	}
+	return 0;
+}
