d95a28dcae8c55f14f3615be7bb0aadd7ca626e1
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-02-13 20:13:48 +0530

    Create Chef and nextgen.cpp

---
+++ Chef and nextgen.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    //Kindly change variable do not copy paste :)
+    while(t--){
+        int a,b,x,y;
+        cin>>a>>b>>x>>y;
+        int c=a*b;
+        int z=x*y;
+        (z>=c)?cout<<"yes":cout<<"no";
+        printf("\n");
+       
+    }
+ 
+ return 0;
+}
